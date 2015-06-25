c7a6c7ec07b2b527405194753413589b274654f9
unknown <urtiz.sam@gmail.com>
2015-06-25 17:49:01 -0500

    reportes controller

---
+++ dcilaboratorio/protected/controllers/ReportesController.php
@@ -0,0 +1,215 @@
+<?php
+
+class ReportesController extends Controller
+{
+	/**
+	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+	 * using two-column layout. See 'protected/views/layouts/column2.php'.
+	 */
+	public $layout='//layouts/column2';
+    public $section = "Reportes";
+    public $subSection="Generar";
+    public $pageTitle="Reportes";
+	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+			'accessControl', // perform access control for CRUD operations
+			'postOnly + delete', // we only allow deletion via POST request
+		);
+	}
+
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules()
+	{
+		return array(
+			array('allow',  // allow all users to perform 'index' and 'view' actions
+				'actions'=>array('index','view'),
+				'users'=>array('*'),
+			),
+			array('allow', // allow authenticated user to perform 'create' and 'update' actions
+				'actions'=>array('create','update'),
+				'users'=>array('@'),
+			),
+			array('allow', // allow admin user to perform 'admin' and 'delete' actions
+				'actions'=>array('admin','delete'),
+				'users'=>array('admin'),
+			),
+			array('allow', // allow admin user to perform 'admin' and 'delete' actions
+				'actions'=>array('admin','generar'),
+				'users'=>array('admin'),
+			),
+			array('deny',  // deny all users
+				'users'=>array('*'),
+			),
+		);
+	}
+
+	/**
+	 * Displays a particular model.
+	 * @param integer $id the ID of the model to be displayed
+	 */
+	public function actionView($id)
+	{
+		$this->render('view',array(
+			'model'=>$this->loadModel($id),
+		));
+	}
+
+	/**
+	 * Creates a new model.
+	 * If creation is successful, the browser will be redirected to the 'view' page.
+	 */
+	public function actionCreate()
+	{
+		$this->subSection = "Nuevo";
+		$model=new TitulosForm;
+
+		// Uncomment the following line if AJAX validation is needed
+		// $this->performAjaxValidation($model);
+
+		if(isset($_POST['TitulosForm']))
+		{
+			$model->attributes=$_POST['TitulosForm'];
+			if($model->save())
+				$this->redirect(array('view','id'=>$model->id));
+		}
+
+		$this->render('create',array(
+			'model'=>$model,
+		));
+	}
+
+	/**
+	 * Updates a particular model.
+	 * If update is successful, the browser will be redirected to the 'view' page.
+	 * @param integer $id the ID of the model to be updated
+	 */
+	public function actionUpdate($id)
+	{
+		$model=$this->loadModel($id);
+
+		// Uncomment the following line if AJAX validation is needed
+		// $this->performAjaxValidation($model);
+
+		if(isset($_POST['TitulosForm']))
+		{
+			$model->attributes=$_POST['TitulosForm'];
+			if($model->save())
+				$this->redirect(array('view','id'=>$model->id));
+		}
+
+		$this->render('update',array(
+			'model'=>$model,
+		));
+	}
+
+	/**
+	 * Deletes a particular model.
+	 * If deletion is successful, the browser will be redirected to the 'admin' page.
+	 * @param integer $id the ID of the model to be deleted
+	 */
+	public function actionDelete($id)
+	{
+		$model=$this->loadModel($id);
+		if(isset($model->activo))
+			$model->activo=$model->activo==0?1:0;
+		else
+			$model->delete();
+		$model->save();	
+
+		$status = (!isset($model->activo)?"Eliminado":($model->activo==0?"Desactivado":"Activado"));
+		echo '{id:'.$model->id.', estatus:'.$status.'}';	
+	}
+
+	/**
+	 * Lists all models.
+	 */
+	public function actionGenerar()
+	{
+		$model=new BusquedaForm;
+		$criteria=new CDbCriteria;
+
+		if(isset($_POST['BusquedaForm']))
+		{
+			$model->attributes=$_POST['BusquedaForm'];
+			
+
+			//$models=Ordenes::model()->findAll();
+			
+			
+			//$criteria->join = 'orden_precio_examen';
+			$criteria->condition = 'id_doctores = :idDoctores';
+        	$criteria->params = array(":idDoctores" => $model['id_doctores'], 
+        							  ":id_multitarifarios"=> $model['id_multitarifarios'],
+        							  );
+			$criteria->addBetweenCondition('creacion', $model['fecha_inicial'], $model['fecha_final']);
+			
+
+
+			/*$models=Ordenes::model()->findAllByAttributes(array(
+			'id_doctores'=>$model['id_doctores'],
+			'id_multitarifarios'=>$model['id_multitarifario'],			
+			),$criteria);
+			print_r($models);*/
+
+
+
+
+			
+			//$resultado = $model->search();
+			
+		}
+		$this->render('generar', array('model'=>$model));
+	}
+
+	/**
+	 * Manages all models.
+	 */
+	public function actionAdmin()
+	{
+		$this->subSection="Admin";
+		$model=new TitulosForm('search');
+		$model->unsetAttributes();  // clear any default values
+		if(isset($_GET['TitulosForm']))
+			$model->attributes=$_GET['TitulosForm'];
+
+		$this->render('admin',array(
+			'model'=>$model,
+		));
+	}
+
+	/**
+	 * Returns the data model based on the primary key given in the GET variable.
+	 * If the data model is not found, an HTTP exception will be raised.
+	 * @param integer $id the ID of the model to be loaded
+	 * @return TitulosForm the loaded model
+	 * @throws CHttpException
+	 */
+	public function loadModel($id)
+	{
+		$model=TitulosForm::model()->findByPk($id);
+		if($model===null)
+			throw new CHttpException(404,'The requested page does not exist.');
+		return $model;
+	}
+
+	/**
+	 * Performs the AJAX validation.
+	 * @param TitulosForm $model the model to be validated
+	 */
+	protected function performAjaxValidation($model)
+	{
+		if(isset($_POST['ajax']) && $_POST['ajax']==='reportes-form')
+		{
+			echo CActiveForm::validate($model);
+			Yii::app()->end();
+		}
+	}
+}
---
+++ dcilaboratorio/protected/models/BusquedaForm.php
@@ -0,0 +1,168 @@
+<?php
+
+/**
+ * LoginForm class.
+ * LoginForm is the data structure for keeping
+ * user login form data. It is used by the 'login' action of 'SiteController'.
+ */
+class BusquedaForm extends CFormModel
+{
+	public $fecha_final;
+	public $fecha_inicial;
+	public $id_pacientes;
+	public $id_doctores;
+	public $id_multitarifario;
+	public $grupo_examen;
+	public $id_grupo;
+	public $dia;
+	public $mes;
+	public $año;
+	public $semana;
+	public $hora;
+	public $folio;
+	public $nombre_paciente;
+	public $unidad;
+	public $doctor;
+	public $id_paciente;
+	public $clave_examen;
+	public $costo;
+	public $porcentaje_descuento;
+	public $monto_descuento;
+	public $tarifa;
+
+	public $check;
+
+
+	/**
+	 * Declares the validation rules.
+	 * The rules state that username and password are required,
+	 * and password needs to be authenticated.
+	 */
+	public function rules()
+	{
+		return array(
+			// username and password are required
+			array('fecha_inicial, fecha_final', 'required'),
+			// boolean
+			array('dia, mes, año, semana, id_paciente, hora, folio, nombre_paciente, unidad, doctor, examen, costo, porcentaje_desc, monto_desc, tarifa', 'boolean'),
+			// numerical
+			array('id_doctores, id_pacientes, id_doctores, id_multitarifarios, clave_examen, id_grupo', 'numerical', 'integerOnly'=>true),
+		);
+	}
+
+	/**
+	 * Declares attribute labels.
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'fecha_inicial'=>'Fecha Inicial',
+			'fecha_final'=>'Fecha Final',
+			'id_pacientes'=>'Paciente',
+			'id_doctores'=>'Doctor',
+			'id_multitarifario'=>'Multitarifario',
+			'id_examenes'=>'Examen',
+			'id_grupo'=>'Grupo de exámen',
+			'dia'=>'Día',
+			'mes'=>'Mes',
+			'año'=>'Año',
+			'semana'=>'Semana',
+			'hora'=>'Hora',
+			'folio'=>'Folio',
+			'nombre_paciente'=>'Nombre del paciente',
+			'unidad'=>'Unidad Responsable',
+			'doctor'=>'Doctor',
+			'examen'=>'Examen',
+			'costo'=>'Costo',
+			'porcentaje_descuento'=>'% Desc',
+			'monto_descuento'=>'Monto descuento',
+			'tarifa'=>'Tarifa',
+
+
+
+		);
+	}
+public function generar()
+	{
+
+}
+
+/* Criteria es para lo q vas a mostrar
+public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('id_doctores',$this->id_doctores);
+		$criteria->compare('id_status',$this->id_status);
+		$criteria->compare('id_unidades_responsables',$this->id_unidades_responsables);
+		$criteria->compare('fecha_captura',$this->fecha_captura,true);
+		$criteria->compare('informacion_clinica_y_terapeutica',$this->informacion_clinica_y_terapeutica,true);
+		$criteria->compare('comentarios',$this->comentarios,true);
+		$criteria->compare('requiere_factura',$this->requiere_factura);
+		$criteria->compare('descuento',$this->descuento);
+		$criteria->compare('id_multitarifarios',$this->id_multitarifarios);
+		$criteria->compare('compartir_con_doctor',$this->compartir_con_doctor);
+		$criteria->compare('costo_emergencia',$this->costo_emergencia,true);
+		$criteria->compare('ultima_edicion',$this->ultima_edicion,true);
+		$criteria->compare('usuario_ultima_edicion',$this->usuario_ultima_edicion);
+		$criteria->compare('creacion',$this->creacion,true);
+		$criteria->compare('usuario_creacion',$this->usuario_creacion);
+		if(Yii::app()->user->getState('perfil')=='Paciente'){
+			$criteria->with=array('ordenFacturacion');
+			$criteria->compare('ordenFacturacion.id_usuarios',Yii::app()->user->id);
+		}
+		if(Yii::app()->user->getState('perfil')=='Doctor'){	
+			$criteria->compare('id_doctores',Yii::app()->user->getState('id_persona'));
+		}
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+			'pagination'=>false,
+		));
+	}
+
+
+
+
+
+	
+	 * Authenticates the password.
+	 * This is the 'authenticate' validator as declared in rules().
+	 */
+	public function search()
+	{
+
+
+		if(!$this->hasErrors())
+		{
+
+			//Crear una instancia del formulario para obtener los valores ingresados por post
+			
+			$model=new BusquedaForm;
+			//echo 'lalalala'+$model['fecha_inicial'];
+
+			//Cómo hacer relaciones o cruzar entre dos modelos: ORdenes tiene exam y como sabre el grupo?
+			/*$criteria->addBetweenCondition('creacion', $model['fecha_inicial'], $model['fecha_fin']);
+
+			$model=Ordenes::model()->findAllByAttributes(array(
+			'id_doctores'=>$idDoctores,
+			'id_multitarifarios'=>$idMultitarifarios,			
+			),$criteria);
+
+			*/
+		}
+		return array();
+	}
+
+	
+
+
+	/**
+	 * Logs in the user using the given username and password in the model.
+	 * @return boolean whether login is successful
+	 */
+	
+
+}
