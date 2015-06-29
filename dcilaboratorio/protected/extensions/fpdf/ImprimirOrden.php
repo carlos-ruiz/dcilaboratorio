bd52afb651b2e7539d49fe121320391af49dada9
unknown <urtiz.sam@gmail.com>
2015-06-28 21:18:58 -0500

    orden con formato pdf

--- dcilaboratorio/protected/extensions/fpdf/ImprimirOrden.php
+++ dcilaboratorio/protected/extensions/fpdf/ImprimirOrden.php
@@ -4,17 +4,18 @@ class ImprimirOrden extends FPDF{
 
 	function Header(){
 		$this->SetFont('Arial','B',18);
-		$this->Image(dirname(__FILE__).DIRECTORY_SEPARATOR.'../../../css\layout\img/gvia_logo22.png',1.5,1.5,2,2);
+		$this->Image(dirname(__FILE__).DIRECTORY_SEPARATOR.'../../../css\layout\img/gvia_logo22.png',1,1.7,2.3,2.3);
     // Move to the right
     //$this->Cell(8);
     // Title
-		$this->SetXY(4, 1);
+		$this->SetXY(4, .75);
 		$this->Cell(0,2.54,'DIAGNOSTICO CLÍNICO INTEGRAL',0,0,'C');
 		$this->ln(0.75);
-		$this->SetFont('Times','',8);
+		$this->SetFont('Times','B',8);
 		$this->SetXY(4, 1.75);
 		$this->Cell(0,2.54,'UNIDAD CENTRAL                    UNIDAD FELIX IRETA                     UNIDAD AMADO NERVO                     UNIDAD DE CANCEROLOGIA',0,0,'C');
 		$this->SetXY(4, 2.30);
+        $this->SetFont('Times','',8);
 		$this->Cell(0,2.54,'Gnl.Bravo #170                                      Tucurán #230                                   Amado Nervo $392-A                                 Francisco Madero #145',0,0,'L');
 		$this->SetXY(3.8, 2.65);
 		$this->Cell(0,2.54,'Col. Chapultepec Nte.                              Col. Félix Ireta                                          Col. Centro                                   Fracc. Ex Gob. Gildardo Magaña',0,0,'L');
@@ -31,22 +32,48 @@ class ImprimirOrden extends FPDF{
 	{
         $y = 0.5;
         $this->SetXY(-5, 6);
+        $this->SetFont('Arial','',8);
         $this->Cell(1,$y,'Fecha:', 0, 0);
-        $this->Cell(2,$y,$model->fecha_captura, 0, 1);
+        $this->SetFont('Arial','B',8);
+        $fecha = explode('-', $model->fecha_captura);
+        $dia= substr($fecha[2], 0, 2);
+        $hora = explode(' ', $fecha[2]);
+        $hora = explode(':', $hora[1]);
+        
+
+        $fecha = $dia.'/'.$fecha[1].'/'.$fecha[0].'   '. $hora[0].':'.$hora[1];
+        $this->Cell(2,$y,$fecha, 0, 1);
         $this->setX(1);
+        $this->SetFont('Arial','B',12);
+        $this->SetTextColor(75, 141, 248);
         $this->Cell(10,$y,'Orden de Trabajo', 0, 1);
+        $this->SetFont('Arial','',8);
+        $this->SetTextColor(0, 0, 0); 
         $this->Cell(3,$y,'Folio:', 0, 0);
+        $this->SetFont('Arial','B',8);
         $this->Cell(3,$y,$model->id, 0, 1);
+        $this->SetFont('Arial','',8);
         $this->Cell(3,$y,'Paciente', 0, 0);
+        $this->SetFont('Arial','B',8);
         $this->Cell(10,$y,$model->ordenFacturacion->paciente->obtenerNombreCompleto(), 0, 0);
         $this->setX(-5);
+        $this->SetFont('Arial','',8);
         $this->Cell(1,$y,'Sexo:', 0, 0);
+        $this->SetFont('Arial','B',8);
         $this->Cell(2,$y,$model->ordenFacturacion->paciente->sexo==0?'Masculino':'Femenino', 0, 1);
         $this->setX(1);
+        $this->SetFont('Arial','',8);
         $this->Cell(3,$y,'Médico:', 0, 0);
-        $this->Cell(10,$y,$model->doctor->obtenerNombreCompleto(), 0, 1);
+        $this->SetFont('Arial','B',8);
+        if($model->doctor)
+            $this->Cell(10,$y,$model->doctor->obtenerNombreCompleto(), 0, 1);
+        else
+            $this->Cell(10,$y,'Sin Dr. asignado', 0, 1);
         $this->ln(0.5);
+        $this->SetFont('Arial','B',12);
+        $this->SetTextColor(75, 141, 248);
         $this->Cell(3,$y,'Estudios Solicitados', 0, 1);
+        $this->SetTextColor(0, 0, 0); 
         
         
         $this->SetXY(1, 10);
@@ -88,6 +115,7 @@ class ImprimirOrden extends FPDF{
             $idExamen = $examen->id;
         }
         $this->setX(16.5);
+        $this->SetFont('Arial','B',8);
         $this->Cell(4,$y,'Total: $'.$totalOrden, 1, 1, 'R');
         $pagos = $model->pagos;
         $totalPagado = 0;
@@ -101,15 +129,22 @@ class ImprimirOrden extends FPDF{
 
         //Observaciones
         $this->ln(1);
+        $this->SetFont('Arial','',8);
         $this->Cell(4,$y,'Observaciones:', 0, 1);
+        $this->SetFont('Arial','B',8);
         $this->Cell(10,$y,$model->comentarios, 0, 1);
         $this->ln(1);
-        $this->Cell(10,$y,'Puede consultar sus resultados por Internet en la siguente liga:', 0, 1);
-        $this->Cell(10,$y,'www.dcilaboratorio.com', 0, 1);
+        $this->SetFont('Arial','B',8);
+        $this->Cell(8.75,$y,'Puede consultar sus resultados por Internet en la siguente liga:', 0, 0);
+        $this->SetFillColor(75, 141, 248);
+        $this->SetFont('Arial','U',8);
+        $this->Cell(5,$y,'www.dcilaboratorio.com', 0, 1);
+        $this->SetFont('Arial','B',10);
         $this->Cell(10,$y,'Usuario: '.$model->ordenFacturacion->usuario->usuario, 0, 1);
         $this->Cell(10,$y,'Contraseña: '.base64_decode($model->ordenFacturacion->usuario->contrasena), 0, 1);
 
         $this->ln(1);
+        $this->SetFont('Arial','B',8);
         $this->Cell(10,$y,'Sus resultados estarán listos en '.$duracion.' días.', 0, 1);
 
     }
