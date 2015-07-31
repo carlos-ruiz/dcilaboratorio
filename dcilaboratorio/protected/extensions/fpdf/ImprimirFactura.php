60c97dde973bca553dfe14ed61aa30caec706cfb
unknown <urtiz.sam@gmail.com>
2015-07-30 20:32:50 -0500

    PDF factura

--- dcilaboratorio/protected/extensions/fpdf/ImprimirFactura.php
+++ dcilaboratorio/protected/extensions/fpdf/ImprimirFactura.php
@@ -4,51 +4,19 @@ class ImprimirFactura extends FPDF{
 
 	function Header(){
         $y = 0.5;
-		$this->SetFont('Arial','B',18);
-		$this->Image(dirname(__FILE__).DIRECTORY_SEPARATOR.'../../../css/layout/img/gvia_logo22.png',1,1.7,2.3,2.3);
+		$this->SetFont('Arial','B',14);
+        $this->Image(dirname(__FILE__).DIRECTORY_SEPARATOR.'../../../css/layout/img/gvia_logo22.png',16,1.7,4.3,3.3);
+		
     // Move to the right
     //$this->Cell(8);
     // Title
-		$this->SetXY(4, .75);
-		$this->Cell(0,2.54,'DIAGNOSTICO CLÍNICO INTEGRAL',0,0,'C');
-		$this->ln(0.75);
-		$this->SetFont('Times','B',8);
-        $this->SetXY(3, 2.75);
-        $this->Cell(4.3, $y, 'UNIDAD CENTRAL', 0, 0, 'C');
-        $this->Cell(4.3, $y, 'UNIDAD FÉLIX IRETA', 0, 0, 'C');
-        $this->Cell(4.3, $y, 'UNIDAD AMADO NERVO', 0, 0, 'C');
-        $this->Cell(4.5, $y, 'UNIDAD DE CANCEROLOGÍA', 0, 1, 'C');
-        $this->SetX(3);
-        $this->SetFont('Times','',8);
-        $y = 0.4;
-        $this->Cell(4.3, $y, 'Gnl.Bravo #170', 0, 0, 'C');
-        $this->Cell(4.3, $y, 'Tucurán #230', 0, 0, 'C');
-        $this->Cell(4.3, $y, 'Amado Nervo #392-A', 0, 0, 'C');
-        $this->Cell(4.5, $y, 'Francisco Madero #145', 0, 1, 'C');
-        $this->SetX(3);
-        $this->Cell(4.3, $y, 'Col. Chapultepec Nte.', 0, 0, 'C');
-        $this->Cell(4.3, $y, 'Col. Félix Ireta', 0, 0, 'C');
-        $this->Cell(4.3, $y, 'Col. Centro', 0, 0, 'C');
-        $this->Cell(4.5, $y, 'Fracc. Ex Gob. Gildardo Magaña', 0, 1, 'C');
-        $this->SetX(3);
-        $this->Cell(4.3, $y, 'C.P. 58260', 0, 0, 'C');
-        $this->Cell(4.3, $y, 'C.P. 58070', 0, 0, 'C');
-        $this->Cell(4.3, $y, 'C.P. 58000', 0, 0, 'C');
-        $this->Cell(4.5, $y, 'C.P. 58149', 0, 1, 'C');
-        $this->SetX(3);
-        $this->Cell(4.3, $y, 'Tel.(443)232-0166', 0, 0, 'C');
-        $this->Cell(4.3, $y, 'Tel.(443)326-9891', 0, 0, 'C');
-        $this->Cell(4.3, $y, 'Tel.(443)312-3490', 0, 0, 'C');
-        $this->Cell(4.5, $y, 'Tel.(443)232-0165', 0, 1, 'C');
+		
+		
+        
 	}
 
 	function cabeceraHorizontal($model, $datosFactura)
 	{
-        $y = 0.5;
-        $this->ln(1);
-        $this->setX(-7.5);
-        $this->SetFont('Arial','B',8);
-        $this->Cell(0,$y,'Fecha:', 0, 1, 'R');
         $fecha = explode('-', $model->fecha_captura);
         $dia= substr($fecha[2], 0, 2);
         $hora = explode(' ', $fecha[2]);
@@ -56,24 +24,75 @@ class ImprimirFactura extends FPDF{
 
 
         $fecha = $dia.'/'.$fecha[1].'/'.$fecha[0].'   '. $hora[0].':'.$hora[1];
+
+        $y = 0.5;
+        $this->SetXY(1, .75);
+        $this->Cell(0,2.54,'SILVIA GUERRA LAUCHINO',0,0,'C');
+        $this->ln(0.5);
+        $this->SetFont('Arial','B',12);
+        $this->Cell(0,2.54,'RFC. GULS720801NY4',0,0,'C');
+        $this->ln(0.75);
+        $this->SetFont('Times','B',14);
+        $this->SetXY(1.8, 2.75);
+        $this->Cell(1.3, $y, 'Factura', 0, 0, 'C');
+        $this->SetFont('Times','B',10);
+        $this->Cell(.9, 2, 'Domicilio y Expendido en:', 0, 0, 'C');
+        $this->SetX(2);
+        $this->SetFont('Times','',8);
+        $this->Cell(1, 3, 'Gnl.Bravo #170', 0, 0, 'C');        
+        $this->Cell(1, 3.7, 'Col. Chapultepec Nte. C.P. 58260', 0, 0, 'C');
+        $this->Cell(-2.4, 4.4, 'Morelia Michoacán', 0, 0, 'C');
+
+        $this->SetXY(2.4, 3.7);
+        $this->SetFont('Times','B',8);
+        $this->Cell(1, 3.7, 'Lugar de expedición:', 0, 0, 'C');
+        $this->SetFont('Times','',8);
+        $this->Cell(3.9, 3.7, 'Morelia, Michoacán', 0, 0, 'C');
+        $this->SetFont('Times','B',8);
+        $this->SetXY(2.4, 4);
+        $this->Cell(.6, 3.8, 'Datos del Receptor', 0, 0, 'C'); 
+        $this->SetXY(1, 4);
+        $this->Cell(2, 4.6, 'Cliente:', 0, 0, 'C'); 
+        $this->SetXY(2, 4);
+        $this->Cell(-0.2, 5.3, 'RFC:', 0, 0, 'C');   
+        $this->Cell(0, 5.5, 'Folio Fiscal: '.$datosFactura['uuid'], 0, 0, 'R');      
+        $this->ln(1.5); 
+
+        $y = 0.5;
+        $this->ln(.75);
+        $this->SetFont('Times','B',9);
+        $this->setX(2.5);
+        $this->Cell(0,0.1,$model->ordenFacturacion->datosFacturacion->razon_social, 0, 1, 'L');
+        $this->Cell(0,0.1,'Comprobante Digital por Internet', 0, 1, 'R');
+        $this->SetFont('Times','',8);
+        $this->setX(2.5);
+        $this->Cell(5,0.6,$model->ordenFacturacion->datosFacturacion->RFC, 0, 1, 'L');
+        $this->ln(.2);
+        $this->SetFont('Times','B',8);
+        $this->setX(1.5);
+        $this->Cell(0,0.1,'Domicilio', 0, 1, 'L');
+        $this->Cell(0,0.1,'Número de comprobante: '.$datosFactura['certNumber'], 0, 1, 'R');
+        $this->SetFont('Times','',8);
+        $this->setX(1.5);
+        $this->Cell(2,0.6,'Calle '.$model->ordenFacturacion->datosFacturacion->direccion->calle.' N° '.$model->ordenFacturacion->datosFacturacion->direccion->numero_ext, 0, 1, 'L');
+        $this->Cell(0,0.1,'Forma de pago: Pago en Una sola exibición', 0, 1, 'R');
+        $this->setX(1.5);
+        $this->Cell(2,0.1,'Col. '.$model->ordenFacturacion->datosFacturacion->direccion->colonia.' CP. '.$model->ordenFacturacion->datosFacturacion->direccion->codigo_postal, 0, 1, 'L');
+        $this->Cell(0,0.4,'Fecha de comprobante '.$fecha, 0, 1, 'R');
+        $this->setX(1.5);
+        $this->Cell(2,-0.1,$model->ordenFacturacion->datosFacturacion->direccion->municipio->nombre.', '.$model->ordenFacturacion->datosFacturacion->direccion->estado->nombre, 0, 1, 'L');
+        $this->setX(1.5);
+        $this->Cell(2,0.8,'Moneda: PESOS   Tipo de cambio: 1.00', 0, 1, 'L');
+        $this->Cell(0,-1,'Fecha de certificación del CFDI '.$datosFactura['date'], 0, 1, 'R');
+
+        $this->setX(-8.5);
+        $this->SetXY(-7.5,9.5);
+        $this->Cell(0,0,'Método de pago y cuenta: TARJETA', 0, 1, 'R');
         $this->setX(-7.5);
-        $this->Cell(0,$y,$fecha, 0, 1, 'R');
+     
         $this->setX(-7.5);
-        $this->Cell(0,$y,'Factura:', 0, 1, 'R');
-        $this->setX(-7.5);
-        $this->Cell(0,$y,'XXXXXX', 0, 1, 'R');
-        $this->setX(-7.5);
-        $this->Cell(0,$y,'Folio fiscal:', 0, 1, 'R');
-        $this->setX(-7.5);
-        $this->Cell(0,$y,$datosFactura['uuid'], 0, 1, 'R');
-        $this->setX(-7.5);
-        $this->Cell(0,$y,'No de serie del certificado:', 0, 1, 'R');
-        $this->setX(-7.5);
-        $this->Cell(0,$y, $datosFactura['certNumber'], 0, 1, 'R');
-        $this->setX(-7.5);
-        $this->Cell(0,$y,'Fecha y hora de emisión:', 0, 1, 'R');
-        $this->setX(-7.5);
-        $this->Cell(0,$y, $datosFactura['date'], 0, 1, 'R');
+        $this->Cell(0,.7,'Regimen Fiscal: Reg. de la PF con Act. Emp. y Prof.', 0, 1, 'R');
+       
 
 
         $this->ln(0.5);
@@ -82,7 +101,7 @@ class ImprimirFactura extends FPDF{
         $this->Cell(3,$y,'Estudios Solicitados', 0, 1);
         $this->SetTextColor(0, 0, 0);
 
-        $this->ln(1);
+        $this->ln(.5);
         $this->SetFont('Arial','B',8);
         $this->SetFillColor(75, 141, 248);//Fondo azul de celda
         $this->SetTextColor(0, 0, 0); //Letra color blanco
@@ -148,10 +167,12 @@ class ImprimirFactura extends FPDF{
 
         $this->ln(2);
         $this->setX(1);
-        $this->MultiCell(0, $y, 'string - '.$datosFactura['string'], 0, 'L', false);
+
+        $this->MultiCell(0, $y, 'Este documento es una representación impresa de un CFDI - '.$datosFactura['string'], 0, 'L', false);
         $this->MultiCell(0, $y, 'cfdStamp - '.$datosFactura['cfdStamp'], 0, 'L', false);
-        $this->MultiCell(0, $y, 'certNumber - '.$datosFactura['certNumber'], 0, 'L', false);
-        $this->MultiCell(0, $y, 'satStamp - '.$datosFactura['satStamp'], 0, 'L', false);
+        $this->MultiCell(0, $y, 'Sello digital del emisor (certNumber) - '.$datosFactura['certNumber'], 0, 'L', false);
+        $this->MultiCell(0, $y, 'Sello digital del SAT(satStamp) - '.$datosFactura['satStamp'], 0, 'L', false);
+         $this->Image(dirname(__FILE__).DIRECTORY_SEPARATOR.'../../../css/layout/img/qr.png',16,22,4,4);
     }
 
     function Footer()
