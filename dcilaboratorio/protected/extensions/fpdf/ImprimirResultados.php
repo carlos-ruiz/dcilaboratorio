52fd1bb4ef8c833f110ed0cac994e9cbaa148866
Carlos Ruiz <car.ruiz90@gmail.com>
2015-09-24 17:08:36 -0500

    Cambios en el reporte de resultados

--- dcilaboratorio/protected/extensions/fpdf/ImprimirResultados.php
+++ dcilaboratorio/protected/extensions/fpdf/ImprimirResultados.php
@@ -101,7 +101,7 @@ class ImprimirResultados extends FPDF{
         $this->Cell(3.5,$y, 'Resultado',1, 0 , 'C', true);
         $this->Cell(2,$y, 'Unidad',1, 0 , 'C', true);
         $this->Cell(5,$y, 'Intervalo de Referencia',1, 0 , 'C', true);
-         $this->SetFillColor(219,239,253);
+        // $this->SetFillColor(219,239,253);
         //Atención!! el parámetro true rellena la celda con el color elegido
 		//$this->Cell(120,7, utf8_decode("hola mundo"),1, 0 , 'C', true);
 		//$this->Cell(20,7, utf8_decode($cabecera[2]),1, 0 , 'C', true);
@@ -118,27 +118,98 @@ class ImprimirResultados extends FPDF{
         $idExamen = 0;
         $totalOrden = 0;
         $duracion = 0;
+        //examenes de la orden
+        $idsExamenes=array();
         foreach ($ordenTieneExamenes as $ordenExamen) {
-            $examen = $ordenExamen->detalleExamen->examenes;
-            if($examen->id!=$idExamen){
-            $this->SetFont('Arial','B',8);
-            $this->Cell(19.5,$y, $examen->tecnica==null?'"'.$examen->nombre.'"':'"'.$examen->nombre.'"  (Técnica empleada: '.$examen->tecnica.')',1, 1 ,'C', true);
-            }
-
-        $this->Cell(9,$y,$ordenExamen->detalleExamen->descripcion ,1, 0 , 'C');
-        if($ordenExamen->resultado > $ordenExamen->detalleExamen->rango_superior || $ordenExamen->resultado < $ordenExamen->detalleExamen->rango_inferior){
-            $this->SetFont('Times','BI',8);
-            $this->SetTextColor(255, 0, 0);
+            array_push($idsExamenes, $ordenExamen->detalleExamen->id_examenes);
         }
-        $this->Cell(3.5,$y,$ordenExamen->resultado,1, 0 , 'C');
-        $this->SetTextColor(0, 0, 0);
-        $this->SetFont('Arial','B',8);
-        $this->Cell(2,$y, $ordenExamen->detalleExamen->unidadesMedida->abreviatura,1, 0 , 'C');
-        $rango=$ordenExamen->detalleExamen->rango_inferior.'-'.$ordenExamen->detalleExamen->rango_promedio.'-'.$ordenExamen->detalleExamen->rango_superior;
-        $this->Cell(5,$y, $rango,1, 1 , 'C');
-        $idExamen = $examen->id;
 
+        //grupos de la orden
+        $grupos = Grupos::model()->findAll();
+        $gruposExistentesEnOrden=array();
 
+        //Obtener grupos incluidos en la orden
+        foreach ($grupos as $grupo) {
+            $examenes=GrupoExamenes::model()->findAll("id_grupos_examenes=?",array($grupo->id));
+            $tiene=true;
+            // print_r($examenes);return;
+            foreach($examenes as $grupoExamen){
+                if(!in_array($grupoExamen->id_examenes, $idsExamenes)){
+                    $tiene=false;
+                    break;
+                }
+            }
+            if($tiene){
+             array_push($gruposExistentesEnOrden,$grupo->id);
+            }
+        }
+
+        $examenesImpresos=array();
+        foreach ($gruposExistentesEnOrden as $grupo) {
+            $grupo = Grupos::model()->findByPk($grupo);
+            $examenesEnGrupo=GrupoExamenes::model()->findAll('id_grupos_examenes=?',array($grupo->id));
+            $examenesIds=array();
+            $this->SetFillColor(117, 163, 240);
+            $this->Cell(19.5,$y, $grupo->nombre ,1, 1, 'C', true);
+
+            foreach ($examenesEnGrupo as $grupoExamen) {
+                array_push($examenesIds, $grupoExamen->id_examenes);
+            }
+            foreach ($ordenTieneExamenes as $ordenExamen) {
+                $examen = $ordenExamen->detalleExamen->examenes;
+                if(in_array($examen->id, $examenesIds)&&!in_array($examen->id, $examenesImpresos)){
+                    //Pintamos el examen
+                    array_push($examenesImpresos, $examen->id);
+                    if($examen->id!=$idExamen){
+                    $this->SetFont('Arial','B',8);
+                    $this->SetFillColor(213, 224, 241);
+                    $this->Cell(19.5,$y, $examen->tecnica==null?'"'.$examen->nombre.'"':'"'.$examen->nombre.'"  (Técnica empleada: '.$examen->tecnica.')',1, 1 ,'C', true);
+                    }
+
+                    $this->Cell(9,$y,$ordenExamen->detalleExamen->descripcion ,1, 0 , 'C');
+                    if($ordenExamen->resultado > $ordenExamen->detalleExamen->rango_superior || $ordenExamen->resultado < $ordenExamen->detalleExamen->rango_inferior){
+                        $this->SetFont('Times','BI',8);
+                        $this->SetTextColor(255, 0, 0);
+                    }
+                    $this->Cell(3.5,$y,$ordenExamen->resultado,1, 0 , 'C');
+                    $this->SetTextColor(0, 0, 0);
+                    $this->SetFont('Arial','B',8);
+                    $this->Cell(2,$y, $ordenExamen->detalleExamen->unidadesMedida->abreviatura,1, 0 , 'C');
+                    $rango=$ordenExamen->detalleExamen->rango_inferior.'-'.$ordenExamen->detalleExamen->rango_promedio.'-'.$ordenExamen->detalleExamen->rango_superior;
+                    $this->Cell(5,$y, $rango,1, 1 , 'C');
+                }
+                $idExamen = $examen->id;
+            }
+        }
+
+        if(sizeof($idsExamenes)!=sizeof($examenesImpresos)){
+            $this->SetFillColor(117, 163, 240);
+            $this->Cell(19.5,$y, "EXÁMENES INDIVIDUALES" ,1, 1, 'C', true);
+        }
+        $idExamenExiste = 0;
+        $examen = null;
+        foreach ($idsExamenes as $idExamen) {
+            if(!in_array($idExamen,$examenesImpresos)){
+                $examen=Examenes::model()->findByPk($idExamen);
+                if($examen->id!=$idExamenExiste){
+                    $this->SetFont('Arial','B',8);
+                    $this->SetFillColor(213, 224, 241);
+                    $this->Cell(19.5,$y, $examen->tecnica==null?'"'.$examen->nombre.'"':'"'.$examen->nombre.'"  (Técnica empleada: '.$examen->tecnica.')',1, 1 ,'C', true);
+                }
+
+                $this->Cell(9,$y,$ordenExamen->detalleExamen->descripcion ,1, 0 , 'C');
+                if($ordenExamen->resultado > $ordenExamen->detalleExamen->rango_superior || $ordenExamen->resultado < $ordenExamen->detalleExamen->rango_inferior){
+                    $this->SetFont('Times','BI',8);
+                    $this->SetTextColor(255, 0, 0);
+                }
+                $this->Cell(3.5,$y,$ordenExamen->resultado,1, 0 , 'C');
+                $this->SetTextColor(0, 0, 0);
+                $this->SetFont('Arial','B',8);
+                $this->Cell(2,$y, $ordenExamen->detalleExamen->unidadesMedida->abreviatura,1, 0 , 'C');
+                $rango=$ordenExamen->detalleExamen->rango_inferior.'-'.$ordenExamen->detalleExamen->rango_promedio.'-'.$ordenExamen->detalleExamen->rango_superior;
+                $this->Cell(5,$y, $rango,1, 1 , 'C');
+            }
+            $idExamenExiste = $examen->id;
         }
 
         //Observaciones
